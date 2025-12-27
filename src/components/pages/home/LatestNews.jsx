410df70b2d0da28b58afe1806160dcaec6078ab1
Keshav-gola <keshavgola285@gmail.com>
2025-12-27 11:13:21 +0530

    add navigate on latest news

--- src/components/pages/home/LatestNews.jsx
+++ src/components/pages/home/LatestNews.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const LatestNews = ({ leftNews }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (slug) => {
+    navigate(`/detail-news/${slug}/myitronline`);
+  };
+
   return (
     <div className="hidden lg:block lg:col-span-3 px-2 h-full ">
       <h1 className="text-3xl font-bold">Latest News</h1>
@@ -8,16 +15,26 @@ const LatestNews = ({ leftNews }) => {
 
       <div className="space-y-4">
         {leftNews.map((item, index) => (
-          <div key={index} className="border-b pb-3 pt-1">
+          <div
+            key={index}
+            onClick={() => handleNavigate(item.slug)}
+            className="border-b pb-3 pt-1"
+          >
             <div className="flex flex-row gap-2 text-sm text-gray-700">
               <span className="font-semibold">{item.category}</span>
               <span className="text-yellow-500">{item.date}</span>
             </div>
 
-            <h3 className="text-lg font-semibold">{item.title.length > 67 ? `${item.title.substring(0, 66)}...`: item.title}</h3>
+            <h3 className="text-lg font-semibold">
+              {item.title.length > 67
+                ? `${item.title.substring(0, 66)}...`
+                : item.title}
+            </h3>
 
             <p className="text-gray-600 text-sm leading-relaxed">
-              {item.excerpt.length > 137 ? `${item.excerpt.substring(0, 137)}...` : item.excerpt}
+              {item.excerpt.length > 137
+                ? `${item.excerpt.substring(0, 137)}...`
+                : item.excerpt}
             </p>
           </div>
         ))}
