10fd87ddf99cd4b2905c69416885237008b90ed8
Mukesh Kumar <mukesh512004@gmail.com>
2026-01-27 11:25:58 +0530

    apiservice changed

--- src/components/context/apiService/apiService.js
+++ src/components/context/apiService/apiService.js
@@ -18,7 +18,7 @@ const localApi = axios.create({
 
 export const searchNews = async (query) => {
   try {
-    const response = await localApi.get(
+    const response = await api.get(
       `/news/search?query=${encodeURIComponent(query)}`
     );
 
@@ -32,7 +32,7 @@ export const searchNews = async (query) => {
 
 export const latestNews = async () => {
   try {
-    const response = await localApi.get("/myitr-news/right-side");
+    const response = await api.get("/myitr-news/right-side");
 
     return response.data;
   } catch (error) {
@@ -43,7 +43,7 @@ export const latestNews = async () => {
 
 export const lendingPageNews = async () => {
   try {
-    const response = await localApi.get("/myitr-news/latest");
+    const response = await api.get("/myitr-news/latest");
     return response.data;
   } catch (error) {
     console.error("Lending Page News API error:", error);
@@ -53,7 +53,7 @@ export const lendingPageNews = async () => {
 
 export const featureData = async () => {
   try {
-    const response = await localApi.get("/myitr-news/featured");
+    const response = await api.get("/myitr-news/featured");
     return response.data;
   } catch (error) {
     console.error("Error while fetching Features: ", error);
@@ -63,7 +63,7 @@ export const featureData = async () => {
 
 export const contactUs = async (data) => {
   try {
-    const response = await localApi.post("contact-us", data);
+    const response = await api.post("contact-us", data);
     return response.data;
   } catch (error) {
     console.error("Contact API error:", error);
@@ -73,7 +73,7 @@ export const contactUs = async (data) => {
 
 export const fetchDetailsNewsApi = async (slug, lng = "en") => {
   try {
-    const response = await localApi.get(`/myitr-news/show/${slug}?lang=${lng}`);
+    const response = await api.get(`/myitr-news/show/${slug}?lang=${lng}`);
 
     if (response.status === 200) {
       return response.data;
@@ -88,7 +88,7 @@ export const fetchDetailsNewsApi = async (slug, lng = "en") => {
 
 export const getCategories = async () => {
   try {
-    const response = await localApi.get("/myitr-news/categories");
+    const response = await api.get("/myitr-news/categories");
     return response.data;
   } catch (error) {
     console.error("Categories API error:", error);
@@ -98,7 +98,7 @@ export const getCategories = async () => {
 
 export const fetchNewsByCategory = async (id, categoryType) => {
   try {
-    const response = await localApi.get(`/myitr-news/by-category-type?category_id=${id}&type=${categoryType}`);
+    const response = await api.get(`/myitr-news/by-category-type?category_id=${id}&type=${categoryType}`);
 
     if (response.status) {
       return response.data
